ccdcd6ae7c71d4efbd96c87038c70700bca5217d
PINK <henghengrahman@gmail.com>
2026-07-22 00:22:52 +0700

    Add files via upload

---
+++ public/js/admin.js
@@ -0,0 +1,1 @@
+const toggle=document.querySelector('.menu-toggle');const sidebar=document.querySelector('.sidebar');if(toggle&&sidebar)toggle.addEventListener('click',()=>sidebar.classList.toggle('open'));
