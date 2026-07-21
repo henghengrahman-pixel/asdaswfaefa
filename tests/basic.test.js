0c80c869a828defa0d4eca337f54642b6820b916
PINK <henghengrahman@gmail.com>
2026-07-22 00:13:38 +0700

    Delete tests directory

--- tests/basic.test.js
+++
@@ -1,6 +0,0 @@
-const test=require('node:test');
-const assert=require('node:assert/strict');
-const {money,slug,stripHtml}=require('../src/helpers');
-test('helper slug aman untuk URL',()=>assert.equal(slug('Cream Kaki Pecah-Pecah!'),'cream-kaki-pecah-pecah'));
-test('format rupiah',()=>assert.match(money(29900),/29\.900/));
-test('hapus HTML',()=>assert.equal(stripHtml('<p>Hello <b>World</b></p>'),'Hello World'));
