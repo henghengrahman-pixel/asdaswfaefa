0722522617c2a0c336e24f58cd2626119801ef2b
PINK <henghengrahman@gmail.com>
2026-09-03 19:50:36 +0700

    Delete public directory

--- public/js/admin.js
+++
@@ -1,1 +0,0 @@
-const toggle=document.querySelector('.menu-toggle');const sidebar=document.querySelector('.sidebar');if(toggle&&sidebar)toggle.addEventListener('click',()=>sidebar.classList.toggle('open'));
